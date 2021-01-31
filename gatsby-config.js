7114db12ae60578c9664d00cabe30ca3d297fa14
treoa <anuar.suleimenov@nu.edu.kz>
2021-01-31 09:06:17 +0600

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "",
+        trackingId: "260569022",
       },
     },
     "gatsby-plugin-sharp",
